74ea1d2c056098f30e17705033988f33cbee475f
Gore Sawata <104631058+GoreSawata27@users.noreply.github.com>
2022-08-21 17:25:51 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,8 @@
 const counterReducer = (state,action) =>{
-
+    switch(action.type){
+        case 'increment': return state+1;
+        case 'decrement': return state-1;
+    }
 }
 
 export {counterReducer}
